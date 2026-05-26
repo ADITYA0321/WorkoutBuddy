12faafe24fe44e5bcc519dc8e84e207d8cf1a2a4
ADITYA0321 <71208484+ADITYA0321@users.noreply.github.com>
2026-05-26 23:09:13 +0530

    Update API endpoint for workout deletion

--- frontend/src/componants/WorkoutDetails.js
+++ frontend/src/componants/WorkoutDetails.js
@@ -9,7 +9,7 @@ import { formatDistanceToNow } from 'date-fns';
 const WorkoutDetails = ({workout}) => {
     const {dispatch} = useWorkoutsContext();
         const handleClick = async () =>{
-            const response = await fetch('/api/workout/' + workout._id, {
+            const response = await fetch('https://workoutbuddy-hgc6.onrender.com/api/workouts' + workout._id, {
                 method: 'DELETE'
             })
                 const json = await response.json()
